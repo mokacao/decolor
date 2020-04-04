aaf860810f0eb11aa3e7e9294e9e102139d0fbfb
yfor <yfor1008@gmail.com>
2020-04-04 17:20:11 +0800

    [update] update result

--- demo.m
+++ demo.m
@@ -11,7 +11,21 @@ gray1 = rtcprgb2gray(im);
 hsv(:,:,3) = gray1;
 im1 = hsv2rgb(hsv);
 
-subplot(221), imshow(im)
-subplot(222), imshow(im1)
-subplot(223), imshow(gray)
-subplot(224), imshow(gray1)
+% subplot(221), imshow(im)
+% subplot(222), imshow(im1)
+% subplot(223), imshow(gray)
+% subplot(224), imshow(gray1)
+
+gray = cat(3, gray, gray, gray);
+gray1 = cat(3, gray1, gray1, gray1);
+
+cmp_img = cat(2, im, gray, gray1);
+[~, w, ~] = size(gray);
+imshow(cmp_img)
+text(10, 10, 'rgb', 'Color', 'red')
+text(10+w, 10, 'rgb2gray', 'Color', 'red')
+text(10+w+w, 10, 'rtcprgb2gray', 'Color', 'red')
+
+fig = getframe;
+figure, imshow(fig.cdata)
+imwrite(fig.cdata, file)
